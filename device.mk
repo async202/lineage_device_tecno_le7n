7a48b2c99256f924dc26dfe70130eb805e7c027a
async202 <96576912+async202@users.noreply.github.com>
2026-08-08 14:11:37 +0700

    le7n: Remove max_frame_buffer_acquired_buffers to eliminate buffer queue starvation in ImageReaderGLOwner

--- device.mk
+++ device.mk
@@ -9,14 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# IMS
+$(call inherit-product, vendor/mediatek/ims/ims.mk)
+
 # A/B
 AB_OTA_PARTITIONS += \
     boot \
-    vbmeta_system \
-    system \
+    dtbo \
     product \
-    vbmeta_vendor \
+    system \
     system_ext \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor \
     vendor
 
 PRODUCT_PACKAGES += \
@@ -38,13 +43,13 @@ PRODUCT_PACKAGES += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=$(BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE) \
     POSTINSTALL_OPTIONAL_system=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=ext4 \
+    FILESYSTEM_TYPE_vendor=$(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE) \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
@@ -59,13 +64,20 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-# Power
+# MTK proprietary power HAL (HIDL, for libPowerHal and vpud)
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.example
+    libmtkperf_client_vendor
+
 
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb.gadget@1.1-service
+
+# Camera / VT / WFD Shims
+PRODUCT_PACKAGES += \
+    libshim_camera_metadata \
+    libshim_sink \
+    libstagefright_hdcp
 
 # Health
 PRODUCT_PACKAGES += \
@@ -82,7 +94,104 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.adb.secure=0 \
     ro.secure=0 \
     ro.debuggable=1 \
-    persist.sys.disable_rescue=true
+    persist.sys.disable_rescue=true \
+    ro.surface_flinger.supports_background_blur=0 \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.nohdcp=1 \
+    media.wfd.use-sw-encoder=1 \
+    debug.stagefright.wfd.encoder=c2.android.avc.encoder \
+    persist.sys.volte_support=1 \
+    telephony.lteOnGsmDevice=1 \
+    ro.telephony.default_network=22,22,22,22 \
+    persist.sys.volte_icon=1 \
+    persist.sys.show_volte_icon=1 \
+    lineage.updater.uri=https://raw.githubusercontent.com/async202/lineageOTA/main/{device}.json
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.mtk_volte_support=1 \
+    persist.vendor.mtk_wfc_support=1 \
+    persist.vendor.volte_support=1 \
+    persist.vendor.mtk.volte.enable=1 \
+    persist.vendor.mtk_ct_volte_support=1 \
+    persist.vendor.ims_support=1 \
+    persist.vendor.mtk_dynamic_ims_switch=1 \
+    persist.vendor.mims_support=2 \
+    ro.vendor.md_auto_setup_ims=1 \
+    persist.vendor.radio.volte_support=1 \
+    persist.sys.volte_support=1 \
+    persist.vendor.radio.mtk_dsbp_support=2 \
+    persist.vendor.radio.fd.counter=15 \
+    persist.vendor.operator.optr=OP18 \
+    persist.vendor.operator.spec=SEGDEFAULT \
+    persist.vendor.operator.seg=DEFAULT \
+    persist.vendor.mtk_operator_id=OP18 \
+    persist.sys.operator.optr=OP18 \
+    ro.vendor.operator.optr=OP18 \
+    ro.vendor.operator.spec=SEGDEFAULT \
+    ro.vendor.operator.seg=DEFAULT \
+    ro.vendor.mtk_operator_id=OP18 \
+    persist.vendor.audio.volte.enable=1 \
+    # persist.dbg.volte_avail_ovr=1 \
+    # persist.dbg.vt_avail_ovr=1 \
+    # persist.dbg.wfc_avail_ovr=1 \
+    persist.vendor.radio.volte_reg_status=1 \
+    persist.vendor.radio.ims_registered=1 \
+    persist.sys.cust.lte_config=1 \
+    persist.vendor.radio.volte_support2=1 \
+    persist.vendor.radio.msimmode=dsds \
+    persist.vendor.radio.mtk_ps1_rat=L/W/G \
+    persist.vendor.radio.mtk_ps2_rat=L/W/G \
+    persist.vendor.radio.smart.data.switch=1 \
+    persist.vendor.radio.ims_act_state=1 \
+    persist.vendor.mtk_ims_video_call=1 \
+    persist.vendor.radio.ims.capability.volte=1 \
+    persist.vendor.radio.ims.capability.vt=1 \
+    persist.vendor.radio.volte_cap=1 \
+    persist.vendor.radio.vilte_cap=1 \
+    persist.vendor.radio.vowfi_cap=1 \
+    persist.vendor.radio.ims.capability.wfc=1 \
+    persist.vendor.volte.enable=1 \
+    persist.vendor.radio.volte.enable=1 \
+    ro.vendor.md_auto_setup_ims=1 \
+    persist.vendor.ims_support=1 \
+    persist.vendor.mtk_dynamic_ims_switch=1 \
+    persist.vendor.mims_support=2 \
+    persist.vendor.volte_support=1 \
+    persist.vendor.mtk.volte.enable=1 \
+    persist.vendor.mtk_wfc_support=1 \
+    persist.vendor.radio.ims_video_precond=0 \
+    persist.vendor.radio.ims_audio_precond=0 \
+    persist.vendor.mtk_ims_precond_support=0 \
+    persist.vendor.radio.disable_precond=1 \
+    persist.vendor.radio.disable_prcd=1 \
+    vendor.net.ims.name=ccmni-ims \
+    vendor.net.ims.ccmni=ccmni0 \
+    vendor.ril.volte.enable=1 \
+    vendor.ril.volte.status=1 \
+    persist.vendor.audio.speech.mic.mode=0 \
+    ro.vendor.audio.single.mic.support=1 \
+    persist.vendor.audio.single.mic.support=1 \
+    persist.vendor.audio.evs.support=1 \
+    persist.vendor.audio.speech.evs.support=1 \
+    vendor.usb.acm_enable=0 \
+    vendor.usb.acm_cnt=0 \
+    vendor.usb.controller=musb-hdrc \
+    ro.vendor.camera.sub_flash=1 \
+    ro.camera.sub_flash=1 \
+    persist.vendor.camera.sub_flash=1 \
+    vendor.camera.sub_flash=1
+
+# System Properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    sys.usb.controller=musb-hdrc \
+    sys.usb.configfs=1
+
+# IMS Service Package
+PRODUCT_PACKAGES += \
+    ImsService \
+    mediatek-ims-base \
+    mediatek-ims-common
+
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -133,6 +242,10 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     libhidltransport.vendor
 
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    device/tecno/le7n/overlay
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/tecno/le7n/le7n-vendor.mk)
 
@@ -141,6 +254,57 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.3-impl \
-    android.hardware.bluetooth.audio@2.0-impl
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default
 PRODUCT_PROPERTY_OVERRIDES += vendor.ril.mtk=1
 PRODUCT_PROPERTY_OVERRIDES += ro.vendor.wlan.gen=gen4m
+PRODUCT_PACKAGES += Camera2 libpcap
+
+# NFC
+PRODUCT_PACKAGES += \
+    NfcNci \
+    Tag \
+    SecureElement \
+    com.android.nfc_extras
+-include device/tecno/le7n/vendor_prop.mk
+-include device/tecno/le7n/config/properties/vendor_logtag.mk
+
+# Media Codecs (Modified VP9 frame-rate limit)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs_mediatek_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_mediatek_video.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Disable AOSP rild
+PRODUCT_COPY_FILES += device/tecno/le7n/rootdir/etc/rild.rc:vendor/etc/init/rild.rc
+
+PRODUCT_COPY_FILES += device/tecno/le7n/rootdir/etc/init.frontal_flash.rc:system/etc/init/init.frontal_flash.rc
+
+# Graphics & Rendering optimizations (from phhuson GSI for MT6768 / Mali-G52)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.egl=mali \
+    persist.vendor.tran_psi_adjust_kill_oom=false \
+    vendor.mtk.vdec.waitkeyframeforplay=9 \
+    ro.vendor.mtk_vdec_waitkeyframeforplay=9 \
+    ro.vendor.mtk.vdec.waitkeyframeforplay=9 \
+    ro.vendor.pref_scale_enable_cfg=1 \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
+    vendor.camera.mdp.cz.enable=1 \
+    vendor.camera.mdp.dre.enable=0 \
+    debug.sf.use_phase_offsets_as_durations=1 \
+    debug.sf.late.sf.duration=30000000 \
+    debug.sf.late.app.duration=24000000 \
+    debug.sf.early.sf.duration=30000000 \
+    debug.sf.early.app.duration=24000000 \
+    debug.sf.earlyGl.sf.duration=30000000 \
+    debug.sf.earlyGl.app.duration=24000000 \
+    debug.sf.hwc.min.duration=23000000 \
+    debug.sf.disable_client_composition_cache=1 \
+    debug.sf.predict_hwc_composition_strategy=0 \
+    debug.sf.enable_gl_backpressure=1 \
+    debug.hwui.renderer=skiagl \
+    debug.renderengine.backend=skiagl \
+    ro.hwui.use_vulkan=0
+
